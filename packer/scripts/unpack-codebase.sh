3cc024dd20b5ca7b344ea6041d00f64422603ad1
Jonathan <jonathan.scruggs@gmail.com>
2022-07-26 21:01:48 -0700

    add build files

---
+++ packer/scripts/unpack-codebase.sh
@@ -0,0 +1,7 @@
+# Create a webapp user
+sudo adduser --disabled-password --gecos "" webapp
+
+# Move the codebase from the tmp directory the app directory
+sudo mv /tmp/codebase/ /var/app/codebase/
+sudo chown -R webapp:webapp /var/app/
+sudo chmod 755 /var/app/codebase
