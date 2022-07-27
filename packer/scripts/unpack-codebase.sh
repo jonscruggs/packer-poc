eef83757ea8f4e24d0ad4e0da7f6a95925f41073
Jonathan <jonathan.scruggs@gmail.com>
2022-07-26 21:53:09 -0700

    disable copy

--- packer/scripts/unpack-codebase.sh
+++ packer/scripts/unpack-codebase.sh
@@ -2,6 +2,6 @@
 sudo adduser --disabled-password --gecos "" webapp
 
 # Move the codebase from the tmp directory the app directory
-sudo mv /tmp/codebase/ /var/app/codebase/
-sudo chown -R webapp:webapp /var/app/
-sudo chmod 755 /var/app/codebase
+#sudo mv /tmp/codebase/ /var/app/codebase/
+#sudo chown -R webapp:webapp /var/app/
+#sudo chmod 755 /var/app/codebase
